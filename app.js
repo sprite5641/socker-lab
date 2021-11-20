89ac8296f4d4c3f62af0dc39f0edc9cc8b3012a3
sprite5641 <prachaya5641.pt@gmail.com>
2021-11-21 02:29:15 +0700

    c4

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ io.on('connection', (socket) => {
 
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
